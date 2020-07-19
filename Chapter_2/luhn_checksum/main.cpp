71dabafcd1e5b9e71c811b931f0c50181b7860b7
MatKie <mtth.kiesel@gmail.com>
2020-07-19 15:03:20 +0100

    Implement doubling of integers for every other number for fixed length of input

--- Chapter_2/luhn_checksum/main.cpp
+++ Chapter_2/luhn_checksum/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 // Program intended to solve Luhn Checksum validation, fixed length of
 // six. See page 36.
-
+int double_int(int digit)
+{
+    int double_digit = 2 * digit;
+    if (double_digit > 9)
+    {
+        return (1 + double_digit % 10);
+    }
+    else
+    {
+        return double_digit;
+    }
+}
 int main()
 {
     // First only read number digit by digit and sum it up.
@@ -11,7 +22,13 @@ int main()
     for (int i = 0; i < 6; i++)
     {
         std::cin >> digit;
-        checksum += (digit - '0');
+        digit -= '0';
+        // Double every other number starting from r.h.s.
+        if (i % 2 != 0)
+        {
+            digit = double_int(digit);
+        }
+        checksum += digit;
     }
     std::cout << checksum << "\n";
 
